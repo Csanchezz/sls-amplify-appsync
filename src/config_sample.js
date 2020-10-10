2db09fedcd5258e39e828dc56f17836117684aa9
csanchezz-sasai <carlos.sanchez@sasai.co>
2020-10-09 20:49:16 -0600

    config added

--- src/config_sample.js
+++ src/config_sample.js
@@ -1,13 +1,13 @@
 export default {
 	cognito: {
-		REGION: 'YOUR_COGNITO_REGION',
-		USER_POOL_ID: 'YOUR_COGNITO_USER_POOL_ID',
-		APP_CLIENT_ID: 'YOUR_COGNITO_APP_CLIENT_ID',
-		IDENTITY_POOL_ID: 'YOUR_IDENTITY_POOL_ID'
+		REGION: 'us-east-1',
+		USER_POOL_ID: 'us-east-1_rGiXxVD6i',
+		APP_CLIENT_ID: '1tsf1378bhcqavanhvoe7nmft8',
+		IDENTITY_POOL_ID: 'us-east-1:1be0d001-0342-4aca-9eab-b849c8df87ca'
 	},
 	graphql: {
-		URL: 'YOUR_GRAPHQL_URL',
-		REGION: 'YOUR_GRAPHQL_REGION',
+		URL: 'https://tbqotcw2czfbbbptkv2ze3vvw4.appsync-api.us-east-1.amazonaws.com/graphql',
+		REGION: 'us-east-1',
 		AUTHENTICATION_TYPE: 'AMAZON_COGNITO_USER_POOLS'
 	}
 };
