50182fe047f446f997c168c6aacad52c04e3aaf5
csanchezz-sasai <carlos.sanchez@sasai.co>
2021-02-15 13:09:41 -0600

    update amplify and add email to attrs

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -45,7 +45,10 @@ export default class Signup extends Component {
 		try {
 			const newUser = await Auth.signUp({
 				username: this.state.email,
-				password: this.state.password
+				password: this.state.password,
+				attributes: {
+					email: this.state.email
+				}
 			});
 			this.setState({
 				newUser
