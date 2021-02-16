47a80f52010c8aa93f62e631e85c9b98e00089ad
csanchezz-sasai <carlos.sanchez@sasai.co>
2021-02-16 14:26:42 -0600

    update

--- src/App.js
+++ src/App.js
@@ -49,8 +49,37 @@ class App extends Component {
 		};
 		return (
 			<div className="App container">
-				<button onClick = {() => Auth.federatedSignIn({provider: "Google", token })} >Google</button>
-				<button onClick = {() => Auth.federatedSignIn()} >Federated</button>
+				<Navbar fluid collapseOnSelect>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<Link to="/">Test application</Link>
+						</Navbar.Brand>
+						<Navbar.Toggle />
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav pullRight>
+							{this.state.isAuthenticated ? (
+								<NavItem onClick={this.handleLogout}>Logout</NavItem>
+							) : (
+								<Fragment>
+									<LinkContainer to="/signup">
+										<NavItem>Signup</NavItem>
+									</LinkContainer>
+									<LinkContainer to="/login">
+										<NavItem>Login</NavItem>
+									</LinkContainer>
+									<LinkContainer to="/facebook">
+										<NavItem>Facebook</NavItem>
+									</LinkContainer>
+									<LinkContainer to="/google">
+										<NavItem>Google</NavItem>
+									</LinkContainer>
+								</Fragment>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				<Routes childProps={childProps} />
 			</div>
 		);
 	}
